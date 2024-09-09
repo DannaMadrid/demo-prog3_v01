84db0d229e9a17fad662ba694da193be7d146d2f
DannaMadrid <danna.madrid28691@ucaldas.edu.co>
2024-09-09 10:55:36 -0500

    implementacion resta

---
+++ main.py
@@ -0,0 +1,10 @@
+def suma (x,y):
+    return x+y
+
+respuesta=suma(5,7)
+print(respuesta)
+
+respuesta = suma(5,7)
+print (respuesta)
+respuesta = respuesta(7-5)
+print ("resultado es: ", respuesta)
